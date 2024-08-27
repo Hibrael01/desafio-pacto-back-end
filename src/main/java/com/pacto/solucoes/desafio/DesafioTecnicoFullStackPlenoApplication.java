6161ee01c7bbf4264616ca44df67862ad79fd037
Hibrael <hibrael46@gmail.com>
2024-08-27 16:30:09 -0300

    Commit inicial para criação do projeto

---
+++ src/main/java/com/pacto/solucoes/desafio/DesafioTecnicoFullStackPlenoApplication.java
@@ -0,0 +1,13 @@
+package com.pacto.solucoes.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoFullStackPlenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoFullStackPlenoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pacto/solucoes/desafio/DesafioTecnicoFullStackPlenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacto.solucoes.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoFullStackPlenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
